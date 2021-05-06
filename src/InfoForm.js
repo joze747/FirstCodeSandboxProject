7a424a5573be1de06e7483c367c71e405b483ca3
joze747 <79260857+joze747@users.noreply.github.com>
2021-05-06 02:32:50 +0200

    Dokončana naloga

--- src/InfoForm.js
+++ src/InfoForm.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
-import DropdownChoice from "./DropdownChoice";
+import { Form, Dropdown } from "semantic-ui-react";
+import returnValue from "./returnValue";
 
 const regijeOptions = [
   { key: "go", value: "go", text: "Gorenjska" },
@@ -24,31 +24,104 @@ const spolOptions = [
 ];
 
 export default function InfoForm() {
+  const [name, setName] = React.useState("");
+  const [surname, setSurname] = React.useState("");
+  const [region, setRegion] = React.useState("");
+  const [sex, setSex] = React.useState("");
+
+  const [submittedName, setSubmittedName] = React.useState(() =>
+    name ? name : ""
+  );
+  const [submittedSurname, setSubmittedsurname] = React.useState(() =>
+    surname ? surname : ""
+  );
+  const [submittedRegion, setSubmittedRegion] = React.useState(() =>
+    region ? region : ""
+  );
+  const [submittedSex, setSubmittedSex] = React.useState(() =>
+    sex ? sex : ""
+  );
+
+  const handleChangeName = (event) => setName(event.target.value);
+  const handleChangeSurname = (event) => setSurname(event.target.value);
+  const handleChangeRegion = (event, data) =>
+    setRegion(returnValue(regijeOptions, data.value));
+  const handleChangeSpol = (event, data) =>
+    setSex(returnValue(spolOptions, data.value));
+
+  const handleSubmit = (event, data) => {
+    setSubmittedName(name);
+    setSubmittedsurname(surname);
+    setSubmittedRegion(region);
+    setSubmittedSex(sex);
+  };
+
+
   return (
-    <Form>
-      <Form.Group>
-        <Form.Field required width={6}>
-          <label>Ime</label>
-          <input placeholder="Ime" />
-        </Form.Field>
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Field required width={6}>
+            <label>Ime</label>
+            <input
+              placeholder="Ime"
+              onChange={handleChangeName}
+              name="name"
+              id="name"
+            />
+          </Form.Field>
 
-        <Form.Field required width={6}>
-          <label>Priimek</label>
-          <input placeholder="Priimek" />
-        </Form.Field>
-      </Form.Group>
+          <Form.Field required width={6}>
+            <label>Priimek</label>
+            <input
+              placeholder="Priimek"
+              onChange={handleChangeSurname}
+              name="surname"
+              id="surname"
+            />
+          </Form.Field>
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Field required width={8}>
-          <label>Regija</label>
-          <DropdownChoice options={regijeOptions} />
-        </Form.Field>
+        <Form.Group>
+          <Form.Field required width={8}>
+            <label>Regija</label>
+            <Dropdown
+              placeholder="Izberi regijo"
+              fluid
+              search
+              onChange={handleChangeRegion}
+              selection
+              options={regijeOptions}
+              id="region"
+            />
+          </Form.Field>
 
-        <Form.Field required width={4}>
-          <label>Spol</label>
-          <DropdownChoice options={spolOptions} />
-        </Form.Field>
-      </Form.Group>
-    </Form>
+          <Form.Field required width={4}>
+            <label>Spol</label>
+            <Dropdown
+              placeholder="Izberi spol"
+              fluid
+              search
+              onChange={handleChangeSpol}
+              selection
+              options={spolOptions}
+              id="sex"
+            />
+          </Form.Field>
+        </Form.Group>
+
+        <Form.Button content="submit" />
+      </Form>
+      <strong>onChange:</strong>
+      <pre>{JSON.stringify({ name, surname, region, sex }, null, 2)}</pre>
+      <strong>onSubmit:</strong>
+      <pre>
+        {JSON.stringify(
+          { submittedName, submittedSurname, submittedRegion, submittedSex },
+          null,
+          2
+        )}
+      </pre>
+    </div>
   );
 }
---
+++ src/returnValue.js
@@ -0,0 +1,9 @@
+function returnValue(options, value) {
+  for (let i = 0; i < options.length; i++) {
+    if (value === options[i].value) {
+      return options[i].text;
+    }
+  }
+  return value;
+}
+export default returnValue;
